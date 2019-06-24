00dbbbbd194b817a162321ad4d56b23948c7137e
jsierra <jsierra@kaufmanrossin.com>
2019-06-23 21:01:14 -0400

    now with the mutation working

--- app/graphql/inputs/user_skill_level_attributes.rb
+++ app/graphql/inputs/user_skill_level_attributes.rb
@@ -2,5 +2,5 @@ Inputs::UserSkillLevelAttributes = GraphQL::InputObjectType.define do
   name("UserSkillLevelAttributes")
   input_field :id, types.String
   input_field :email, types.String
-  input_field :skill_levels, types[Inputs::SkillLevelAttributes]
+  input_field :skill_level, Inputs::SkillLevelAttributes
 end
--- app/graphql/mutations/create_user_skill_levels.rb
+++ app/graphql/mutations/create_user_skill_levels.rb
@@ -3,11 +3,24 @@ module Mutations
 
     argument :user_skills, [Inputs::UserSkillLevelAttributes], required: true
 
-    field :user_skill_level, Types::UserSkillLevelType, null: true
+    field :user_skill_levels, [Types::UserSkillLevelType], null: true
     field :errors, [Types::UserError], null: true
 
-    def resolve(users:, skill_levels:)
-      puts "the sklslkflkssdf", users.inspect, skill_levels.inspect
+    def resolve(user_skills:)
+      user_ids = user_skills.map{|arg| {id: arg.id , level_id: arg.skill_level[:level_id], skill_id: arg.skill_level[:skill_id]}}
+
+      usr_skill_levels = user_skills.map do |arg|
+        UserSkillLevel.find_or_create_by!({
+          user: User.find_by(id: arg.id),
+          skill_level: SkillLevel.find_or_create_by!({
+              level_id: arg.skill_level[:level_id],
+              skill_id: arg.skill_level[:skill_id]
+            })
+        })
+      end
+      {
+        user_skill_levels: usr_skill_levels
+      }
     end
   end
 end
